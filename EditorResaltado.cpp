2c309b0efcfce64283bd67a2bfc753c62fca9b4d
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2024-06-21 18:21:13 -0300

    Agregamos nuevas funciones

--- EditorResaltado.cpp
+++ EditorResaltado.cpp
@@ -39,20 +39,65 @@ const set<id_comm>& EditorResaltado::comentarios_palabra(unsigned pos) const {
 	// TODO: implementar y justificar complejidad
 
 	set<id_comm> ids_comentarios = this->_comentarios_de_cada_palabra[pos];
-
 }
 
-void EditorResaltado::insertar_palabra(const string& palabra, unsigned pos) {
-	// TODO: implementar y justificar complejidad
+void EditorResaltado::insertar_palabra(const string& palabra, unsigned pos) { 
+    this->_texto.insert(_texto.begin() + pos, palabra);
+
+    if (!this->_comentarios_de_cada_palabra[pos].empty()){
+        set<id_comm> aux = this->_comentarios_de_cada_palabra[pos];
+        this->_comentarios_de_cada_palabra.insert(this->_comentarios_de_cada_palabra.begin() + pos, aux);
+    } else {
+    	this->_comentarios_de_cada_palabra.insert(this->_comentarios_de_cada_palabra.begin() + pos, set<id_comm>());
+    }
+    
+    this->_longitud_texto++;
 }
+
 
 void EditorResaltado::borrar_palabra(unsigned pos) {
 	// TODO: implementar y justificar complejidad
+	auto it_texto = this->_texto.begin();
+	auto it_comentario = this->_comentarios_de_cada_palabra.begin();
+	int i = 0;
+
+	while(it_texto != this->_texto.end() && it_comentario != this->_comentarios_de_cada_palabra.end()) {
+		if(i == pos) {
+			this->_texto.erase(it_texto);
+			if(it_comentario->empty()) {
+				this->_comentarios_de_cada_palabra.erase(it_comentario);
+			} else {
+				this->_borrar_palabra_aux(pos); 
+			}
+		}
+		++it_texto; ++it_comentario;
+		i++;
+	}
+	
+	this->_longitud_texto--;
 }
 
 id_comm EditorResaltado::comentar(const string& texto, unsigned pos_desde, unsigned pos_hasta) {
 	// TODO: implementar y justificar complejidad
-	return 0;
+	id_comm nuevo;
+	if (this->_comentario.empty()){
+		id_comm first = 1;
+		this->_comentario[first] = texto;
+	} else{
+		id_comm ultimo_id_comm = 0;
+    	// Iterar a través del map para encontrar el último id_comm
+		for (auto it = this->_comentario.begin(); it != this->_comentario.end(); ++it) {
+			if (it->first > ultimo_id_comm) {
+				ultimo_id_comm = it->first;
+			}
+		}
+		nuevo = ultimo_id_comm + 1;
+		this->_comentario[nuevo] = texto;
+	}
+
+	for (int k = pos_desde; k <= pos_hasta; k++){
+		this->_comentarios_de_cada_palabra[k].insert(nuevo);
+	}
 }
 
 void EditorResaltado::resolver_comentario(id_comm id) {
@@ -66,5 +111,21 @@ unsigned EditorResaltado::cantidad_comentarios() const {
 
 EditorResaltado EditorResaltado::con_texto(const string& texto) {
 	// TODO: implementar y justificar complejidad
-	return EditorResaltado();
+	EditorResaltado editor = EditorResaltado();
+	int i = 0;
+	int j = 0;
+
+	while(i < texto.size()) {
+		if(texto[i] != ' ') {
+			editor._texto[j].push_back(texto[i]); // agrego una letra a la palabra
+		} else {
+			editor._texto.push_back(""); // agrego la palabra
+			editor._longitud_texto++;
+			j++;
+		}
+		i++;
+	}
+
+	return editor;
 }
+
